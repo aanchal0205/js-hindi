0bfb8d9b05feb7f18350c39597c0cbc03c1cf96a
aanchal0205 <85793709+aanchal0205@users.noreply.github.com>
2025-07-15 19:00:33 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,10 @@
+const accountId=144553
+let accountEmail="aanchal@gmail.com"
+var accountPassword="Jaipur"
+// accountId=2
+
+// console.log(accountId)
+
+let accountState
+
+console.table([accountId,accountEmail,accountPassword,accountState])
