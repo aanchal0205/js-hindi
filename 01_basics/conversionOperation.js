486be23c2ab0ace8356a61adc66540c696468c54
aanchal0205 <85793709+aanchal0205@users.noreply.github.com>
2025-07-16 19:22:07 +0000

    conversionOperations

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,56 @@
+// let score ="33abc"
+// console.log(typeof score)
+// console.log(typeof(score))
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// let sc =null
+// console.log(typeof sc)
+// console.log(typeof(sc))
+// let valueIn = Number(sc)
+// console.log(typeof valueIn)
+// console.log(valueIn) //null converted to number zero
+
+
+// let score3 =undefined
+// console.log(typeof score3)
+// console.log(typeof(score3))
+// let val = Number(score3)
+// console.log(typeof val)
+// console.log(val) 
+
+
+// let score4 =true
+// console.log(typeof score4)
+// console.log(typeof(score4))
+// let value = Number(score4)
+// console.log(typeof value)
+// console.log(value) 
+
+
+
+// let isLoggedIn=1
+// let booleanIsLoggedIn= Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+
+// let isLoggedIn=""
+// let booleanIsLoggedIn= Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+
+
+// let isLoggedIn="Aanchal"
+// let booleanIsLoggedIn= Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
