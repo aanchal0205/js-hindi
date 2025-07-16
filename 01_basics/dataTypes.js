4815c5f37643756b2a6e81b54690690d4aeece73
aanchal0205 <85793709+aanchal0205@users.noreply.github.com>
2025-07-16 18:26:24 +0000

    container config done

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,11 @@
+"use strict" //treat all JS code as new version
+
+// alert("hello")// we are using node js not browser
+
+
+let name="Aanchal"//String data type
+let age=32 //number
+let isLoggedIn=true //boolean
+
+
+console.log(typeof undefined)
