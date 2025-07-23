d380c9cb5fb21977b09859622171b72b6635c04f
aanchal0205 <85793709+aanchal0205@users.noreply.github.com>
2025-07-23 16:53:49 +0000

    container config done

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -14,12 +14,12 @@
 // console.log(valueIn) //null converted to number zero
 
 
-// let score3 =undefined
+//let score3 =undefined
 // console.log(typeof score3)
 // console.log(typeof(score3))
-// let val = Number(score3)
-// console.log(typeof val)
-// console.log(val) 
+//let val = Number(score3)
+//console.log(typeof val)
+//console.log(val) 
 
 
 // let score4 =true
@@ -31,13 +31,13 @@
 
 
 
-// let isLoggedIn=1
+//  let isLoggedIn=1
 // let booleanIsLoggedIn= Boolean(isLoggedIn)
 // console.log(typeof booleanIsLoggedIn)
 // console.log(booleanIsLoggedIn)
 
 
-// let isLoggedIn=""
+//let isLoggedIn=""
 // let booleanIsLoggedIn= Boolean(isLoggedIn)
 // console.log(typeof booleanIsLoggedIn)
 // console.log(booleanIsLoggedIn)
@@ -50,7 +50,59 @@
 // console.log(booleanIsLoggedIn)
 
 
-let someNumber=33
-let stringNumber=String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let someNumber=33
+// let stringNumber=String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+//**************Operations**************
+
+let value=3
+let negValue=-value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%3)
+
+
+// let str1="hello"
+// let str2=" Aanchal"
+// let str3= str1+str2
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+// console.log((3+4)*(5%3))
+
+// console.log(+true)
+// console.log(+"")
+
+// let num1,num2,num3
+// num1=num2=num3=2+2
+// console.log(num1);
+
+// let gamecounter=100
+// for(let i=0;i<5;i++)
+// {
+// console.log(++gamecounter)
+// }
+// console.log(gamecounter);
+
+// console.log("********");
+
+// let game=100;
+// for(let j=0;j<5;j++)
+// {
+//     console.log(game++);
+    
+// }
+// console.log(game);
+
+
