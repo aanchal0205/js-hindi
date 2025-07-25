384953f8d700c36324f353bef3287ed2d377d935
aanchal0205 <85793709+aanchal0205@users.noreply.github.com>
2025-07-25 10:26:57 +0000

    container config done

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,30 @@
+const score=100
+const scoreval=100.3
+const isloggedin=false;
+const temp=null;
+let userEmail;
+
+// Symbol
+const id=Symbol('123')
+// const anotherId=Symbol('123')
+// console.log(id===anotherId);
+
+
+// Arrays
+const heroes=["Aanchal","Akshay"]
+let myObj={
+name: "Aanchal",
+Age:32
+}
+
+// Function
+
+const myFunction=function(){
+    console.log("Hello World");
+    
+}
+
+console.log(typeof myFunction);
+console.log(typeof myObj);
+console.log(typeof id);
+
